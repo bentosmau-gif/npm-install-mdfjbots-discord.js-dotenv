ab5340bfd37ae7e5025e4e6e6c85a28110dede31
bentosmau-gif <bentosmau@gmail.com>
2026-02-12 15:30:41 -0300

    discord.mb.js
    Server in version 22.22.20 in Node,js.

---
+++ discord.mb.js
@@ -0,0 +1,4 @@
+¡Welcome to Mdfjbots!
+For this file to work, download Node.js v22.22.20 at: nodejs.org
+Then, install in node.js: npm install discord.js and create index.js, package.json, etc.
+Once that's done, go back and install the server.mb file.
